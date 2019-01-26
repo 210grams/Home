da2c91a9a288a47b5253504240e1b8a182871e50
Dan <dan.jeffries3@gmail.com>
2019-01-26 01:27:53 +0000

    PlayerPrefab

--- Assets/Scripts/Player_Interact.cs
+++ Assets/Scripts/Player_Interact.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player_Interact : MonoBehaviour {
 
+    public bool objectHeld;
+    public GameObject heldObj;
+    public string heldObjString;
+    public GameObject objectHolder;
+    public Transform objHolderTransform;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,22 @@ public class Player_Interact : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (!objectHeld)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity) && hit.transform.tag == "Item")
+            {
+                //PickUp tooltip
+                if (Input.GetKeyDown("E"))
+                {
+                    heldObj = hit.transform.gameObject;
+                    Destroy(hit.transform.gameObject);
+                    heldObjString = heldObj.transform.name;
+                    heldObj = (GameObject)Instantiate(Resources.Load(heldObjString), objHolderTransform.parent);
+                }
+            }
+
+            }
+        }
 	}
 }
